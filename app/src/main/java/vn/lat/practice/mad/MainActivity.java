050fe736110c3a1f1e7fad67918e983fde8e85c6
letuss004 <letuss004@gmail.com>
2021-11-01 23:55:43 +0700

    First commit

---
+++ app/src/main/java/vn/lat/practice/mad/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.lat.practice.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
